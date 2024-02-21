30cc2a93de30bb41433d6ba32b74d542726314b5
WenyunHe <87848655+WenyunHe@users.noreply.github.com>
2024-02-21 15:13:50 -0700

    Update appdatabase.py

--- src/appdatabase.py
+++ src/appdatabase.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 class CompetitorNews(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     OEM = db.Column(db.String(255), unique=True, nullable=False)
